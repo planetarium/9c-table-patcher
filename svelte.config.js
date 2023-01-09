1dc0c8d5ab85fac919ee1d8d773d40e0763b6ec9
hyeon <hyeon@planetariumhq.com>
2023-01-09 17:24:47 +0900

    Change settings for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/9c-table-patcher" : "",
+    }
   },
 
   preprocess: [
