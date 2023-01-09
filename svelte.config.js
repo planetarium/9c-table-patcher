28ef15b9d42c70a4e517f7a78f18f2bae0d961d9
hyeon <hyeon@planetariumhq.com>
2023-01-09 16:19:33 +0900

    test app for amplify

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+};
+
+export default config;
