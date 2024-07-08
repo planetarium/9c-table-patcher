d89bd1edb4cc3d841391d8f2d8aa6ef2491b899f
hyeon <hyeon@planetariumhq.com>
2024-07-08 11:51:01 +0900

    Comment out idun: test ended

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,10 +43,10 @@
         "name": "Heimdall (Internal)",
         "url": "https://heimdall-internal-rpc-1.nine-chronicles.com/graphql"
       },
-      "0x100000000002": {
-        "name": "Idun (Internal)",
-        "url": "https://idun-internal-rpc-1.nine-chronicles.com/graphql"
-      }
+      // "0x100000000002": {
+      //   "name": "Idun (Internal)",
+      //   "url": "https://idun-internal-rpc-1.nine-chronicles.com/graphql"
+      // }
     },
     "mainnet": {
       "0x000000000000": {
